dc85d270e220478f2ae9e0e57d534b7708ef8ba8
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-05-23 07:02:36 +0530

    End of section 13

--- src/features/profiles/profilePage/ProfilePage.jsx
+++ src/features/profiles/profilePage/ProfilePage.jsx
@@ -2,40 +2,38 @@ import { useDispatch, useSelector } from "react-redux";
 import { Grid } from "semantic-ui-react";
 import ProfileContent from "./ProfileContent";
 import ProfileHeader from "./ProfileHeader";
-import useFirestoreDoc from '../../../app/hooks/useFirestoreDoc'
+import useFirestoreDoc from "../../../app/hooks/useFirestoreDoc";
 import { getUserProfile } from "../../../app/firestore/firestoreService";
-import { listenToSelectedUserProfile} from '../profileActions'
-import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { listenToSelectedUserProfile } from "../profileActions";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
+export default function ProfilePage({ match }) {
+  const dispatch = useDispatch();
+  const { selectedUserProfile } = useSelector((state) => state.profile);
+  const { currentUser } = useSelector((state) => state.auth);
+  const { loading, error } = useSelector((state) => state.async);
 
-export default function ProfilePage({match}){
+  useFirestoreDoc({
+    query: () => getUserProfile(match.params.id),
+    data: (profile) => dispatch(listenToSelectedUserProfile(profile)),
+    deps: [dispatch, match.params.id],
+  });
 
-    const dispatch = useDispatch();
-    const {selectedUserProfile} = useSelector((state) => state.profile);
-    const {currentUser} = useSelector((state)=>state.auth);
-    const {loading,error} = useSelector((state) => state.async);
+  if ((loading && !selectedUserProfile) || (!selectedUserProfile && !error))
+    return <LoadingComponent content="Loading profile" />;
 
-    useFirestoreDoc({
-        query:() => getUserProfile(match.params.id),
-        data:profile => dispatch(listenToSelectedUserProfile(profile)),
-        deps:[dispatch, match.params.id]
-    })
-
-    if((loading && !selectedUserProfile) || (!selectedUserProfile && !error)) return <LoadingComponent content="Loading profile" />
-
-    return (
-        <Grid>
-            <Grid.Column width={16}>
-                <ProfileHeader
-                 profile={selectedUserProfile} 
-                isCurrentUser={currentUser.uid === selectedUserProfile.id}
-                />
-                <ProfileContent 
-                profile={selectedUserProfile} 
-                isCurrentUser={currentUser.uid === selectedUserProfile.id}
-                
-                />
-            </Grid.Column>
-        </Grid>
-    )
+  return (
+    <Grid>
+      <Grid.Column width={16}>
+        <ProfileHeader
+          profile={selectedUserProfile}
+          isCurrentUser={currentUser.uid === selectedUserProfile.id}
+        />
+        <ProfileContent
+          profile={selectedUserProfile}
+          isCurrentUser={currentUser.uid === selectedUserProfile.id}
+        />
+      </Grid.Column>
+    </Grid>
+  );
 }
